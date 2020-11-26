8e9b72b911709575c370d8298e9e205a5d7b07d5
Hamza Errechydy <61232801+hamzaerrechydyhacker@users.noreply.github.com>
2020-11-26 16:37:31 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+let line = "####"; 
+
+for(let n=0; n<8; n++){
+  if(n % 2 == 0){
+     console.log(" "+ line); 
+  }else{
+     console.log(line); 
+  }
+}
+
+/*
+
+ ####
+####
+ ####
+####
+ ####
+####
+ ####
+####
+
+*/
